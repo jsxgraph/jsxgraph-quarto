fdf453098c14e75fd0c6290f9f9158a662b656a9
Carsten <carsten.miller@uni-bayreuth.de>
2026-02-20 22:47:28 +0100

    reorganisation internal svg code

--- _extensions/jsxgraph/resources/mjs/part_3_chrome.mjs
+++
@@ -1,4 +0,0 @@
-async function main() {
-        const browser = await puppeteer.launch({headless: "new"});
-        const page = await browser.newPage();
-        await page.setViewport({width: parseInt(width), height: parseInt(height)});
